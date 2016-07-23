21ea10196d9afbbc4707a5321370103d3aed8f1d
shonnadorsey2016 <shonna.dorsey@gmail.com>
2016-07-23 12:38:07 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   $('img, header, footer').fadeIn(5000);
-  $('header').click(function(){
-    $('header').css("background", "green");
-});
+    $('header').click(function(){
+      $('header').css("background", "green");
+    });
+  $('button').click(function(){
+    $('img').hide();
+  });
 });
