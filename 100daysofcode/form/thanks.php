d05fd5c991aad0d638277a9ff5a421c5ef686ef5
shonnadorsey2016 <shonna.dorsey@gmail.com>
2020-01-01 19:57:41 -0600

    Create thanks.php

---
+++ 100daysofcode/form/thanks.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Thank you!</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Allura&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" />    
+  </head>
+  <body>
+    <h1>Thank you!</h1>
+    <p>Your message has been sent.</p>
+    
+    <h1>Oops!</h1>
+    <p>There was a problem sending your message.</p>
+  </body>
+</html>    
